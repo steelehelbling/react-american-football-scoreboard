7cd76642ad3c6bbc1bd56174879f5478a996bf03
Steele Helbling <steelehelbling@gmail.com>
2020-03-16 18:03:37 -0400

    ver-1

--- src/App.js
+++ src/App.js
@@ -2,11 +2,20 @@
 import React from "react";
 import "./App.css";
 import BottomRow from "./BottomRow";
+import {useState} from "react"
 
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
+  const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  
+
+//setpoints = (point + 3)
+
+
 
   return (
+
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
@@ -15,12 +24,12 @@ function App() {
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
-            <div className="home__score">32</div>
+            <div className="home__score">{count}</div>
           </div>
-          <div className="timer">00:03</div>
+  <div className="timer">00:03</div>
           <div className="away">
             <h2 className="away__name">Tigers</h2>
-            <div className="away__score">32</div>
+            <div className="away__score">{count2}</div>
           </div>
         </div>
         <BottomRow />
@@ -28,13 +37,14 @@ function App() {
       <section className="buttons">
         <div className="homeButtons">
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown">Home Touchdown</button>
-          <button className="homeButtons__fieldGoal">Home Field Goal</button>
+          <button onClick={()=> setCount(count + 3)} className="homeButtons__touchdown">Home Touchdown</button>
+          <button onClick={()=> setCount(count + 5)} className="homeButtons__fieldGoal">Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown">Away Touchdown</button>
-          <button className="awayButtons__fieldGoal">Away Field Goal</button>
+          <button onClick={()=> setCount2(count2 + 3)} className="awayButtons__touchdown">Away Touchdown</button>
+          <button onClick={()=> setCount2(count2 + 5)} className="awayButtons__fieldGoal">Away Field Goal</button>
         </div>
+
       </section>
     </div>
   );
--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,44 @@
 import React from "react";
 import "./App.css";
+import {useState} from "react"
 
 const BottomRow = () => {
+
+  const [countdown, setCountdown] = useState(0);
+  const [counttoGo, setCounttoGo] = useState(0);
+  const [countballOn, setCountballOn] = useState(0);
+  const [countQuarter, setCountQuarter] = useState(0);
+
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{countdown}</div>
+        <div className="awayButtons">
+          <button onClick={()=> setCountdown(countdown + 1)} className="awayButtons__touchdown">down</button>
+        </div>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{counttoGo}</div>
+        <div className="awayButtons">
+          <button onClick={()=> setCounttoGo(counttoGo + 1)} className="awayButtons__touchdown">toGo</button>
+        </div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+  <div className="ballOn__value">{countballOn}</div>
+        <div className="awayButtons">
+          <button onClick={()=> setCountballOn(countballOn + 1)} className="awayButtons__touchdown">ballOn</button>
+        </div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{countQuarter}</div>         
+         <div className="awayButtons">
+          <button onClick={()=> setCountQuarter(countQuarter + 1)} className="awayButtons__touchdown">Quarter</button>
+        </div>
       </div>
     </div>
   );
